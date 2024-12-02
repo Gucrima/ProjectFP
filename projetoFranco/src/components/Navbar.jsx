60937b2647811f0b650a648e45ef17bc0ee78cdb
Gustavo <gusssoares09@gmail.com>
2024-12-01 21:30:14 -0500

    add Maps

--- projetoFranco/src/components/Navbar.jsx
+++ projetoFranco/src/components/Navbar.jsx
@@ -6,10 +6,10 @@ function Navbar() {
   return (
     <nav className="navbar">
         <img src="./Images/logoSemPistola.png" alt="" />
-        <Link to="/">Home</Link>
-        <Link to="/Agendamento">Agendamento</Link>
-        <Link to="/Historia">História</Link>
-        <Link to="/Servicos">Serviços</Link>
+        <a href="/">Home</a>
+        <a href="/Agendamento">Agendamento</a>
+        <a href="/Historia">História</a>
+        <a href="/Servicos">Serviços</a>
     </nav>
     
   )
--- projetoFranco/src/pages/Agendamento.jsx
+++ projetoFranco/src/pages/Agendamento.jsx
@@ -12,41 +12,55 @@ function Contato() {
       <Navbar />
 
       <div className="conteiner-contato">
-      
-
         <div className="texto-contato">
           <h1>Entre em Contato!</h1>
-          <h1>Seu Carro Como Novo! 🚗💥✨</h1>
-          <h3>
+          <h1>Seu Carro Como Novo! </h1>
+
+          {/* <h3>
             Você sabia que um para-choque danificado ou uma pintura desgastada podem prejudicar o visual e até a segurança do seu carro?
             Aqui na Franco-Parachoques, oferecemos serviços completos de reparo de para-choques, funilaria de qualidade e pintura impecável, tudo feito por profissionais especializados e com materiais de primeira linha!
-          </h3>
-
+          </h3> */}
 
           <div className="texto-contato1">
-          <h2>🔧 Conserto de Para-choques</h2>
-          <h2>⚒️ Funilaria Profissional</h2>
-          <h2>🎨 Pintura Automotiva de Alta Qualidade</h2>
-          <h2>✔️ Atendimento rápido e eficiente</h2>
-          <h2>✔️ Preços acessíveis</h2>
-          <h2>✔️ Garantia de serviço</h2>
-          <h2>✔️ Prazos rápidos</h2>
+            <h2>🔧 Conserto de Para-choques</h2>
+            <h2>⚒️ Funilaria Profissional</h2>
+            <h2>🎨 Pintura Automotiva de Alta Qualidade</h2>
+            <h2>✔️ Atendimento rápido e eficiente</h2>
+            <h2>✔️ Preços acessíveis</h2>
+            <h2>✔️ Garantia de serviço</h2>
+            <h2>✔️ Prazos rápidos</h2>
           </div>
 
           <h3>
             Deixe seu carro como novo! Entre em contato agora mesmo e agende seu orçamento sem compromisso. Não perca tempo, seu carro merece o melhor!
           </h3>
           <h2>Transforme o visual do seu carro e dê um trato na sua segurança com a FrancoParachoques!</h2>
-
         </div>
-          <div className="botaoZap">
-          <a href={linkZap} target="_blank" rel="noopener noreferrer">
-            <img src="./Images/botao-whatsapp-entrar-contato-grd.webp" alt="Botão para WhatsApp" />
+
+        <div className="maps">
+
+            <div className="conteiner-localizacao">
+              <img className="foto_maps" src="./Images/Localizacao_maps.png" alt="Localização no mapa" />
+            </div>
+
+
+            <div className="localizacao-linkmaps"> 
+          <a href='https://maps.app.goo.gl/QvNYQhgADwjmAqhH9' target="_blank" rel="noopener noreferrer">
+            <img src="./Images/png_localizacao-removebg-preview.png" alt="Botão Localização" />
+            Acesse nossa localização no Google Maps 
           </a>
+            </div>
+
+
+          <div className="botaoZap">
+            <a href={linkZap} target="_blank" rel="noopener noreferrer">
+              <img src="./Images/botao-whatsapp-entrar-contato-grd.webp" alt="Botão para WhatsApp" />
+            </a>
           </div>
+        </div>
       </div>
 
-      <Footer/>
+      <Footer />
     </div>
   );
 }
